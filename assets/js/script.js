48ac5b56120a7ccaeb75c66c1bd6e3a0f9dbba19
Emma Olson <emma.olson794@gmail.com>
2022-04-11 11:47:10 -0500

    initial html structure

---
+++ assets/js/script.js
@@ -0,0 +1,5 @@
+console.log("hi");
+
+// WAITING FOR API KEY TO BE ACTIVATED
+// lat & lon are currently hardcoded for san diego california
+var api = "https://api.openweathermap.org/data/2.5/onecall?lat=32.828035&lon=-117.280420&exclude=minutely&appid=8450bd340817d310b29bc7a4282140ff";
