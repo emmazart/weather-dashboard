c2c17a2a1f88d4d6b91e692ad30b59d2b006ac6e
Emma Olson <emma.olson794@gmail.com>
2022-04-15 10:30:49 -0500

    add background formatting to uvi

--- assets/js/script.js
+++ assets/js/script.js
@@ -20,7 +20,6 @@ $(document).ready(function(){
     // on click of search button
     $(searchBtn).click(function(event) {
         event.preventDefault();
-        console.log(this);
 
         // select sibling textarea and return the value
         var input = $(this).siblings("#input").val();
@@ -29,7 +28,7 @@ $(document).ready(function(){
         var userInput = input.charAt(0).toUpperCase() + input.slice(1)
 
         // update header with location selection
-        cityHeader.innerHTML = "<h2 id='city-header'>" + userInput + "<span id='today'>" + formatToday + "</span></h2>"
+        cityHeader.innerHTML = "<h2 id='city-header'>" + userInput + "<span id='today'>" + formatToday + "</span><img id='icon' /></h2>"
 
 
         // ---------- SET & GET CITIES LOCAL STORAGE ---------- //
@@ -42,7 +41,6 @@ $(document).ready(function(){
             localStorage.setItem("cities", JSON.stringify(lsCities));
 
             // call function to create buttons
-            btnList.innerHTML = "";
             createBtns();
 
         // else if nothing is in local storage
@@ -50,7 +48,6 @@ $(document).ready(function(){
             savedCitiesArr.push(userInput);
             lsCities = localStorage.setItem("cities", JSON.stringify(savedCitiesArr));
 
-            btnList.innerHTML = "";
             createBtns();
         }
 
@@ -77,7 +74,9 @@ $(document).ready(function(){
 // ---------- FETCH DATA BASED ON COORDINATES ---------- //
 var coordinateFetch = function(lat, lon) {
 
+    // ---------- POPULATE TODAY'S WEATHER ---------- //
     // select span elements to hold generated data
+    var iconEl = document.querySelector("#icon");
     var tempEl = document.querySelector("#temp");
     var windEl = document.querySelector("#wind");
     var humidEl = document.querySelector("#humid");
@@ -90,20 +89,35 @@ var coordinateFetch = function(lat, lon) {
         return response.json();
     })
         
+    // ---------- GENERATE 5 DAY FORECAST ---------- //
     .then(function(data) {
         console.log(data);
 
         // grab relevant data points
-        var currentTemp = data.current.temp + " F";
+        var currentIcon = data.current.weather[0].icon;
+        // var iconUrl = "http://openweathermap.org/img/wn/" + currentIcon + "@2x.png";
+        var iconUrl = "http://openweathermap.org/img/wn/10d.png"
+
+        var currentTemp = data.current.temp + " °F";
         var currentWind = data.current.wind_speed + " MPH";
         var currentHumid = data.current.humidity + " %";
         var currentUV = data.current.uvi;
 
-        // add data to corresponding elements
+        // add data to corresponding elements for today's weather
+        iconEl.innerHTML = "<img id='icon' src=" + iconUrl + " alt='weather icon' />";
         tempEl.textContent = currentTemp;
         windEl.textContent = currentWind;
         humidEl.textContent = currentHumid;
         uviEl.textContent = currentUV;
+
+        // format background color for UVI using bootstrap classes
+        if (currentUV < 3) {
+            uviEl.classList = "rounded bg-success" // green background
+        } else if (currentUV >= 3 && currentUV <= 5) {
+            uviEl.classList = "rounded bg-warning" // yellow background
+        } else {
+            uviEl.classList = "rounded bg-danger" // red background
+        }
 
         // grab 5 day forecast data
         var forecastArr = data.daily;
@@ -111,13 +125,13 @@ var coordinateFetch = function(lat, lon) {
 
         // iterate through forecastArr
         for (i = 0; i < 5; i++) {
-            var forecastDayData = today.add(i, 'day');
+            var forecastDayData = today.add((i + 1), 'day');
             var forecastDay = dayjs(forecastDayData).format('MM/DD/YYYY');
-            var forecastTemp = "Temp: " + forecastArr[i].temp.day + " F";
+            var forecastTemp = "Temp: " + forecastArr[i].temp.day + " °F";
             var forecastWind = "Wind: " + forecastArr[i].wind_speed + " MPH";
             var forecastHumid = "Humidity: " + forecastArr[i].humidity + " %";
 
-            // package data?
+            // package data
             var forecastObj = {
                 0: forecastDay,
                 1: forecastTemp,
@@ -158,11 +172,10 @@ var generateForecast = function(array) {
     }
 }
 
-
-
-
 // ---------- DYNAMICALLY CREATE BUTTONS FOR SAVED CITIES ---------- //
+
 var createBtns = function() {
+    btnList.innerHTML = "";
     let lsCities = JSON.parse(localStorage.getItem("cities"));
 
     for (var c of lsCities) {
@@ -172,6 +185,3 @@ var createBtns = function() {
         btnList.appendChild(newBtn);
     }
 }
-
-// will need a click listnere for the button list to read text content
-// and determine how to populate the weather data from a save
